cf8c533cb4eb4998107ed9d65acd1c1d68347587
Somtochukwu Ezerioha <Somtochukwu.ezerioha@wemabank.com>
2020-12-07 15:13:13 +0100

    Just added some comments for clarity

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import './App.css';
+
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 const finalSpaceCharacters = [
   {
@@ -31,47 +32,56 @@ const finalSpaceCharacters = [
 ]
 
 function App() {
-  const [characters, updateCharacters] = useState(finalSpaceCharacters);
-
+  // We'll be updating our new characters state
+  const [characters, updateCharacters ] = useState(finalSpaceCharacters);
   function handleOnDragEnd(result) {
-    if (!result.destination) return;
-
-    const items = Array.from(characters);
-    const [reorderedItem] = items.splice(result.source.index, 1);
+    // console.log(result);
+    const items  = Array.from(characters);
+    console.log("Current-items", items);
+    const [reorderedItem ] = items.splice(result.source.index, 1); 
+    console.log("reordered-item", reorderedItem);
     items.splice(result.destination.index, 0, reorderedItem);
+    console.log("New-items", items);
 
     updateCharacters(items);
   }
-
   return (
     <div className="App">
       <header className="App-header">
         <h1>Final Space Characters</h1>
         <DragDropContext onDragEnd={handleOnDragEnd}>
           <Droppable droppableId="characters">
-            {(provided) => (
-              <ul className="characters" {...provided.droppableProps} ref={provided.innerRef}>
-                {characters.map(({id, name, thumb}, index) => {
-                  return (
-                    <Draggable key={id} draggableId={id} index={index}>
-                      {(provided) => (
-                        <li ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
-                          <div className="characters-thumb">
-                            <img src={thumb} alt={`${name} Thumb`} />
-                          </div>
-                          <p>
-                            { name }
-                          </p>
-                        </li>
-                      )}
-                    </Draggable>
-                  );
-                })}
-                {provided.placeholder}
-              </ul>
-            )}
+            {
+              (provided) => (
+                <ul className="characters" {...provided.droppableProps} ref={provided.innerRef}>
+                  {characters.map(({ id, name, thumb }, index) => {
+                    return (
+                      <Draggable key={id} draggableId={id} index={index} >
+                        {
+                          (provided) => (
+                            <li  ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                              <div className="characters-thumb">
+                                <img src={thumb} alt={`${name} Thumb`} />
+                              </div>
+                              <p>
+                                {name}
+                              </p>
+                            </li>
+                          )
+                        }
+                      </Draggable>
+
+                    );
+                  })}
+                  {provided.placeholder}
+                </ul>
+              )
+            }
+
           </Droppable>
+
         </DragDropContext>
+
       </header>
       <p>
         Images from <a href="https://final-space.fandom.com/wiki/Final_Space_Wiki">Final Space Wiki</a>
